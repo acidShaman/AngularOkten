437baa587fe850f31e8eb184afb3eb5dd937c8fa
acid_shaman <andrewmotruk80@gmail.com>
2020-05-05 12:47:42 +0300

    cannot handle httpResponse

--- src/app/services/can-activate.service.ts
+++ src/app/services/can-activate.service.ts
@@ -2,25 +2,28 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {UserService} from './user.service';
+import {UserModel} from '../models/UserModel';
+import {DataService} from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanActivateService  implements CanActivate{
-  constructor(private userService: UserService) {
+
+
+  constructor(private userService: UserService, private dataService: DataService) {
   }
   // tslint:disable-next-line:max-line-length
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const id = route.queryParamMap.get('idOfUser');
-
-    this.userService.getUsers().subscribe(allUsers => {
-      if (!allUsers.find(user => user.id == id)) {
-        console.log('User doesn\'t exist');
-        return false;
-      } else {
-        console.log('you may see your user');
-        return true;
-      }
-    });
+   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean>{
+    const id = +route.queryParamMap.get('idOfUser');
+    const user = await this.userService.getUserById(id).toPromise();
+    if (Object.keys(user).length) {
+      console.log('User exists');
+      this.dataService.addToDB(user);
+      return Promise.resolve(true);
+    } else {
+      console.log('User doesn\'t exist');
+      return Promise.resolve(false);
+    }
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnChanges, SimpleChanges} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {UserModel} from '../models/UserModel';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService{
 
-  constructor() { }
+  state = new BehaviorSubject(null);
+
+  constructor() {
+  }
+
+
+  addToDB(user) {
+    this.state.next(user);
+    console.log(JSON.stringify(this.state.value));
+    localStorage.setItem(`user #${this.state.value.id}`, JSON.stringify(this.state.value));
+  }
 }
